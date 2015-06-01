ae2747bceda5cee015778593c309aafbf28af5ae
cdupont <corentin.dupont@gmail.com>
2015-06-01 21:16:40 +0200

    new version

--- src/Migrate.hs
+++ src/Migrate.hs
@@ -6,17 +6,17 @@ import Data.SafeCopy
 import Control.Applicative
 import Data.Serialize.Put
 import Data.Serialize.Get
-import qualified MigrateV0
+import qualified MigrateV0 as PreviousVersion
 
-data MyType = MyType Int deriving (Show)
+data MyType = MyType Int Int deriving (Show)
 
 instance SafeCopy MyType where
   version = 2
   kind = extension
-  putCopy (MyType n) = contain $ safePut n
-  getCopy = contain $ MyType <$> safeGet
+  putCopy (MyType a b) = contain $ do safePut a; safePut b
+  getCopy = contain $ MyType <$> safeGet <*> safeGet
 
 instance Migrate MyType where
-  type MigrateFrom MyType = MigrateV0.MyType
-  migrate (MigrateV0.MyType n) = MyType (fromIntegral n)
+  type MigrateFrom MyType = PreviousVersion.MyType
+  migrate (PreviousVersion.MyType a) = MyType a 0
 
